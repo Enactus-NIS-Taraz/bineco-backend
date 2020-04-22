062e406a0f4e3ada34e893d576c966455c8fd165
Arsen Saliev <arsensaliev03@gmail.com>
2020-04-22 14:26:11 +0600

    feat: add model users

--- model/users.js
+++ model/users.js
@@ -1,2 +1,29 @@
-// Model users
-module.exports = () => {};
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const userSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    surName: { type: String, required: true },
+    firstName: { type: String, required: true },
+    permission: {
+        chat: { C: Boolean, R: Boolean, U: Boolean, D: Boolean },
+        news: { C: Boolean, R: Boolean, U: Boolean, D: Boolean },
+        settings: { C: Boolean, R: Boolean, U: Boolean, D: Boolean },
+    },
+    accessToken: String,
+    accessTokenExpiredAt: Date,
+    refreshToken: String,
+    refreshTokenExpiredAt: Date,
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+    const password = this.password;
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, password, (err, success) => {
+            if (err) return reject(err);
+            return resolve(success);
+        });
+    });
+};
+
+module.exports = model("User", userSchema);
