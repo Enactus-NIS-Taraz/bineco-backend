b27c1e86ed09f310e116042cff3d8678fffa15d2
Arsen Saliev <arsensaliev03@gmail.com>
2020-04-23 12:20:08 +0600

    added password encryption

--- models/users.js
+++ models/users.js
@@ -13,9 +13,23 @@ const userSchema = new Schema({
     },
 });
 
+userSchema.pre("save", function (next) {
+    bcrypt.hash(this.passwordHash, 10, (err, hash) => {
+        this.passwordHash = hash;
+        next();
+    });
+});
+
+userSchema.pre("update", function (next) {
+    bcrypt.hash(this.passwordHash, 10, (err, hash) => {
+        this.passwordHash = hash;
+        next(this.passwordHash);
+    });
+});
+
 // Сравнить пороли
 userSchema.methods.comparePassword = function (candidatePassword) {
-    const password = this.password;
+    const password = this.passwordHash;
     return new Promise((resolve, reject) => {
         bcrypt.compare(candidatePassword, password, (err, success) => {
             if (err) return reject(err);
