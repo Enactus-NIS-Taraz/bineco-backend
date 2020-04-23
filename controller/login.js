88e24fb05a46a0480fca829295f0df22a6ddc842
Arsen Saliev <arsensaliev03@gmail.com>
2020-04-23 16:46:16 +0600

    feat: add error handler for login

--- controller/login.js
+++ controller/login.js
@@ -1,23 +1,29 @@
 const UserModel = require("../models/users");
 const genToken = require("../helpers/token");
 module.exports = async (req, res) => {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const user = await UserModel.findOne({ email });
-    const isPasswordMatch = UserModel.comparePassword(password);
+        const user = await UserModel.findOne({ email });
+        const isPasswordMatch = UserModel.comparePassword(password);
 
-    if (!user || !isPasswordMatch) {
-        return res.status(400).json({ message: "Неверный email или пороль" });
+        if (!user || !isPasswordMatch) {
+            return res
+                .status(400)
+                .json({ message: "Неверный email или пороль" });
+        }
+
+        const payload = {
+            id: user._id,
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+        };
+
+        const token = genToken(payload);
+
+        return res.status(200).json({ user, ...token });
+    } catch (error) {
+        res.status(500).json({ message: "Ошибка сервера" });
     }
-
-    const payload = {
-        id: user._id,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-    };
-
-    const token = genToken(payload);
-
-    return res.status(200).json({ user, ...token });
 };
