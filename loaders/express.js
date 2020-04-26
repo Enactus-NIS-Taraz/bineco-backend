5dcadcf8bc4381738c29ef90e5f346bcbff9511f
Crepiks <sayazhan.onlassyn@mail.ru>
2020-04-26 18:56:04 +0600

    feat: enable CORS

--- loaders/express.js
+++ loaders/express.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const cors = require("./cors");
 const connectDB = require("./db");
+const bodyParser = require("./bodyParser");
 const auth = require("../middlewares/passport");
-const bodyParser = require("./body-parser");
 const router = require("../routes/index");
 
 module.exports = () => {
-    // Создание экземпляра приложения
-    const app = express();
-    // Подключения к базе данных
-    connectDB();
-    // Парсинг формы
-    bodyParser(app);
-    // Инициализация passport
-    auth.initialize();
-    // Подключения маршрутизаторов
-    router(app);
+  // Создание экземпляра приложения
+  const app = express();
+  // Настройка CORS
+  cors(app);
+  // Подключения к базе данных
+  connectDB();
+  // Парсинг формы
+  bodyParser(app);
+  // Инициализация passport
+  auth.initialize();
+  // Подключения маршрутизаторов
+  router(app);
 
-    return app;
+  return app;
 };
