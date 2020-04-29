3d95d659ab65e5e9425adf3363becb86d3f89893
Arsen Saliev <arsensaliev03@gmail.com>
2020-04-29 12:22:43 +0600

    add error handler for show and create devices

--- controller/devices.js
+++ controller/devices.js
@@ -2,23 +2,31 @@ const deviceModel = require("../models/devices");
 
 module.exports = {
     show: async (req, res) => {
-        const { email } = req.body;
+        try {
+            const { email } = req.body;
 
-        const devices = await deviceModel.find({ owner: email });
+            const devices = await deviceModel.find({ owner: email });
 
-        res.status(200).json(devices);
+            res.status(200).json(devices);
+        } catch (error) {
+            res.status(error.status || 500).json({ message: "Ошибка сервера" });
+        }
     },
     create: async (req, res) => {
-        const { x, y, fullness, active, owner } = req.body;
-        const data = {
-            location: { x, y },
-            status: { fullness, active },
-            owner,
-        };
-        const newDevice = new deviceModel(data);
+        try {
+            const { x, y, fullness, active, owner } = req.body;
+            const data = {
+                location: { x, y },
+                status: { fullness, active },
+                owner,
+            };
+            const newDevice = new deviceModel(data);
 
-        await newDevice.save();
-        res.status(201).json({ success: true });
+            await newDevice.save();
+            res.status(201).json({ success: true });
+        } catch (error) {
+            res.status(error.status || 500).json({ message: "Ошибка сервера" });
+        }
     },
     update: async () => {},
     delete: async () => {},
