4c492f8182064be3901e34fdf18994243d85dcc5
Arsen Saliev <arsensaliev03@gmail.com>
2020-04-29 11:51:59 +0600

    feat: add create new device controller

--- controller/devices.js
+++ controller/devices.js
@@ -2,7 +2,18 @@ const deviceModel = require("../models/devices");
 
 module.exports = {
     show: async () => {},
-    create: async () => {},
+    create: async (req, res) => {
+        const { x, y, fullness, active, owner } = req.body;
+        const data = {
+            location: { x, y },
+            status: { fullness, active },
+            owner,
+        };
+        const newDevice = new deviceModel(data);
+
+        await newDevice.save();
+        res.json({ success: true });
+    },
     update: async () => {},
     delete: async () => {},
 };
