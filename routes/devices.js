767bca1f1b735b5e561b9045692062ea7b42df9c
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-03 14:11:39 +0600

    feat: add auth middleware for devices

--- routes/devices.js
+++ routes/devices.js
@@ -1,8 +1,17 @@
 const deviceController = require("../controller/devices");
+const { authenticate } = require("../middlewares/passport");
 
 module.exports = (app) => {
-    app.get("/api/v1/devices", deviceController.show);
-    app.post("/api/v1/devices/create", deviceController.create);
-    app.patch("/api/v1/devices/:deviceId", deviceController.update);
-    app.delete("/api/v1/devices/:deviceId", deviceController.delete);
+    app.get("/api/v1/devices", authenticate, deviceController.show);
+    app.post("/api/v1/devices/create", authenticate, deviceController.create);
+    app.patch(
+        "/api/v1/devices/:deviceId",
+        authenticate,
+        deviceController.update
+    );
+    app.delete(
+        "/api/v1/devices/:deviceId",
+        authenticate,
+        deviceController.delete
+    );
 };
