667024a7d46d9338fed14190612f9e8fbe8a0fd5
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-03 12:43:26 +0600

    fix: endpoints devices url

--- routes/devices.js
+++ routes/devices.js
@@ -3,6 +3,6 @@ const deviceController = require("../controller/devices");
 module.exports = (app) => {
     app.get("/api/v1/devices", deviceController.show);
     app.post("/api/v1/devices/create", deviceController.create);
-    app.patch("/api/v1/devices/update", deviceController.update);
-    app.delete("/api/v1/devices/delete", deviceController.delete);
+    app.patch("/api/v1/devices/:deviceId", deviceController.update);
+    app.delete("/api/v1/devices/:deviceId", deviceController.delete);
 };
