2e6dda4a79b8b8bdc49a69ed74f1ab97174101f3
Crepiks <sayazhan.onlassyn@mail.ru>
2020-05-21 20:30:36 +0600

    fix: add port option to .env.example

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  secretKey: process.env.SECRET_KEY || "very secret key:)",
+  secretKey: process.env.SECRET_KEY,
   dbUrl: process.env.DB_ADDRESS,
   prefix: "/api/v1",
 };
