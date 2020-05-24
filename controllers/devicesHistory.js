adbd66899d891b195e5e483dab96d3a56f0c7cbd
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-24 10:47:10 +0600

    feat: add delete request for deviceHistory

--- controllers/devicesHistory.js
+++ controllers/devicesHistory.js
@@ -16,4 +16,18 @@ router.get("/", authenticate, async (req, res) => {
   }
 });
 
+router.delete("/:deviceId", async (req, res) => {
+  try {
+    const { deviceId } = req.params;
+    const deletedHistory = await DeviceHistory.findByIdAndDelete(deviceId);
+    res.status(200).json({ deletedDevice });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: true,
+      message: error.message || "Произошла какая-та ошибка",
+    });
+  }
+});
+
 module.exports = router;
