b9ebf527cad1b4883228829ab51b75fa988ab8d8
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-24 22:00:12 +0600

    feat: add error handler for show workplace

--- controllers/workplace.js
+++ controllers/workplace.js
@@ -7,7 +7,13 @@ router.get("/", async (req, res) => {
     const workplace = await Workplace.find({});
 
     res.status(200).json({ workplace });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: true,
+      message: error.message,
+    });
+  }
 });
 
 router.post("/", async (req, res) => {
