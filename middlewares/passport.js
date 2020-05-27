be3957fa959255048f12079bbea90b7da9003d96
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-27 12:33:24 +0600

    refactor: passport init move in loaders

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,12 +1,5 @@
 const config = require("../config/config");
 const passport = require("passport");
-const { ExtractJwt, Strategy } = require("passport-jwt");
-const User = require("../models/User");
-
-const initialize = () => {
-  passport.use("jwt", getStrategy());
-  return passport.initialize();
-};
 
 const authenticate = (req, res, next) => {
   try {
@@ -41,30 +34,6 @@ const authenticate = (req, res, next) => {
   }
 };
 
-const getStrategy = () => {
-  const params = {
-    secretOrKey: config.secretKey,
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  };
-
-  return new Strategy(params, async (payload, done) => {
-    await User.findById(payload.id, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-
-      if (!user) {
-        return done(null, false, {
-          message: "The user in the token was not found",
-        });
-      }
-
-      return done(null, user);
-    });
-  });
-};
-
 module.exports = {
-  initialize,
   authenticate,
 };
