725163199e9f174c5b2fc8fd3a504b41451e06ce
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-29 23:45:05 +0600

    fix: manage rewrite on devices

--- abilities/user.js
+++ abilities/user.js
@@ -6,7 +6,7 @@ function defineAbilitiesFor(user) {
   // can read devices
   can("read", "devices");
   // can manage (i.e., do anything) own posts
-  can("manage", "BlogPost", { author: user._id });
+  can("manage", "devices", { author: user._id });
   // cannot delete a post if it was created more than a day ago
   cannot("delete", "devices");
 
