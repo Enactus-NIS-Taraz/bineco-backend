bc69bca0691329305947ebb9ede68cfb1618b6a0
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-29 23:42:17 +0600

    feat: add define ability for user

---
+++ abilities/user.js
@@ -0,0 +1,16 @@
+const { AbilityBuilder, Ability } = require("@casl/ability");
+
+function defineAbilitiesFor(user) {
+  const { can, cannot, rules } = new AbilityBuilder();
+
+  // can read devices
+  can("read", "devices");
+  // can manage (i.e., do anything) own posts
+  can("manage", "BlogPost", { author: user._id });
+  // cannot delete a post if it was created more than a day ago
+  cannot("delete", "devices");
+
+  return new Ability(rules);
+}
+
+module.exports = defineAbilitiesFor;
