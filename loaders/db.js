73352172b3c03222bd57bd8abe17266e94d7d409
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-30 12:24:25 +0600

    feat: add plugin cals/mongoose for mongoose

--- loaders/db.js
+++ loaders/db.js
@@ -1,22 +1,24 @@
+const { accessibleRecordsPlugin } = require("@casl/mongoose");
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+mongoose.plugin(accessibleRecordsPlugin);
 
-const config = require('../config/config');
+const config = require("../config/config");
 
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 };
 
 module.exports = () => {
-    mongoose
-        .connect(config.dbUrl, options)
-        .then(() => {
-            console.log("MongoDB connected...");
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+  mongoose
+    .connect(config.dbUrl, options)
+    .then(() => {
+      console.log("MongoDB connected...");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
